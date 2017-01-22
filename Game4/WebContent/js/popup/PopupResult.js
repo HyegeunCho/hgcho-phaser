24d6f6d5ec061a322e6193f662aa18071356c6d6
ys-PC\ys <youngsun.yoo@sundaytoz.com>
2017-01-22 15:18:27 +0900

    [추가] 결과창에 점수 나오도록 추가

--- Game4/WebContent/js/popup/PopupResult.js
+++ Game4/WebContent/js/popup/PopupResult.js
@@ -2,6 +2,7 @@ function PopupResult() {
 	
 }
 
+var scoreResultText;
 /** @type Phaser.State */
 var proto = Object.create(Phaser.State);
 PopupResult.prototype = proto;
@@ -11,18 +12,24 @@ preload : function(){
 	
 },
 
-init : function(game){
+init : function(game, start){
 	PopupResult.prototype.scene = new PopupResultResource(game);
 	PopupResult.prototype.scene.visible = false;
 	
 	PopupResult.prototype.scene.fBtn_game_close.inputEnalbed = true;
-	PopupResult.prototype.scene.fBtn_game_close.events.onInputDown.add(Start.prototype.exitGame, this);
+	PopupResult.prototype.scene.fBtn_game_close.events.onInputDown.add(Start.prototype.exitGame, start);
 	
 	PopupResult.prototype.scene.fBtn_popup_restart.inputEnalbed = true;
-	PopupResult.prototype.scene.fBtn_popup_restart.events.onInputDown.add(Start.prototype.restartGame, this);
+	PopupResult.prototype.scene.fBtn_popup_restart.events.onInputDown.add(Start.prototype.restartGame, start);
+	
+	scoreResultText = window.game.add.bitmapText(240, 280, 'textScore', '0', 30);
+	scoreResultText.anchor.set(0.5);
+	scoreResultText.visible = false;
 },
 
-show : function(){
+show : function(score){
+	scoreResultText.visible = true;
+	scoreResultText.text = score;
 	PopupResult.prototype.scene.visible = true;
 },
 
